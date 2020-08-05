92e3f684c9000c8ddeb63adb1bfa49ba3cb17f54
bouillealpha <69195292+bouillealpha@users.noreply.github.com>
2020-08-05 19:49:27 +0200

    Update shortcuts.py

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -43,11 +43,14 @@ def redirect(to, *args, permanent=False, **kwargs):
 
 def _get_queryset(klass):
     """
-    Return a QuerySet or a Manager.
-    Duck typing in action: any class with a `get()` method (for
-    get_object_or_404) or a `filter()` method (for get_list_or_404) might do
-    the job.
+    empty docstring
     """
+#     """
+#     Return a QuerySet or a Manager.
+#     Duck typing in action: any class with a `get()` method (for
+#     get_object_or_404) or a `filter()` method (for get_list_or_404) might do
+#     the job.
+#     """
     # If it is a model class or anything else with ._default_manager
     if hasattr(klass, '_default_manager'):
         return klass._default_manager.all()
